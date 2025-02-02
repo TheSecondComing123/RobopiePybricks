d5c6747f4cd2b4a048ad6eb11b896392091fc22c
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2025-02-02 18:24:31 -0500

    Update run_4.py

--- run_4.py
+++ run_4.py
@@ -1,13 +1,43 @@
 from boilerplate import *
 
 robot = Robot()
+
+# Alignment: 2
+# Shark
 robot.reset_lift_arm_port()
-robot.forward(180)
-robot.turn(20)
-robot.forward(360 * 2)
+robot.forward(180, speed_mm_s=720)
+robot.turn(40)
+robot.forward(240)
 robot.turn(-45)
-robot.forward(50)
-robot.turn(-10)
-robot.forward(75)
+robot.forward(60)
+robot.turn(-40)
+robot.forward(45)
 robot.run_lift_arm(-360)
 robot.reset_lift_arm_port()
+
+# Coral
+robot.forward(-90)
+robot.turn(140)
+robot.forward(-90)
+
+# Coral
+robot.forward(30)
+robot.turn(-90)
+robot.forward(120)
+robot.turn(35)
+robot.forward(-25)
+robot.run_lift_arm(-360)
+robot.reset_lift_arm_port()
+
+# Anglerfish
+robot.turn(-45)
+robot.forward(-90)
+robot.turn(80)
+robot.forward(350)
+robot.run_lift_arm(-360)
+robot.turn(180)
+
+# Back to home base left
+robot.forward(270)
+robot.turn(-40)
+robot.forward(360 * 1.25)
