5a212b3c10e481de787dc7072beeed230217bfa4
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2025-02-08 18:08:53 -0500

    Update run1.py

--- run1.py
+++ run1.py
@@ -2,6 +2,14 @@ from boilerplate import *
 
 robot = Robot()
 
+robot.reset_lift_arm_port()
 robot.forward(169)
-robot.turn(115)
-robot.forward(-310)
+robot.turn(100)
+robot.forward(-320)
+
+robot.forward(25)
+robot.turn(-40)
+robot.run_lift_arm(-450)
+robot.forward(120)
+robot.run_lift_arm(360, speed_dps=180, wait=False)
+robot.turn(90, turn_rate_deg_s=180)
