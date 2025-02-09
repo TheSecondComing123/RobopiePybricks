a606e692a99e57fbaf9090c7f650816e95e19382
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2025-02-09 15:53:49 -0500

    Create stick.py

---
+++ stick.py
@@ -0,0 +1,22 @@
+from boilerplate import *
+
+robot = Robot()
+robot.run_lift_arm(-360)
+robot.forward(100)
+robot.turn(-45)
+robot.forward(100)
+robot.turn(20)
+robot.forward(200)
+robot.turn(80)
+robot.reset_lift_arm_port()
+robot.forward(40, speed_mm_s=360)
+robot.run_lift_arm(-360)
+robot.forward(-100, speed_mm_s=360)
+
+robot.reset_lift_arm_port()
+robot.turn(-55)
+robot.forward(125)
+robot.turn(-17.5)
+robot.forward(35)
+robot.run_lift_arm(-360)
+robot.forward(-500)
